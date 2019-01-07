73f22ea802c369c5da27468667c05cc9781bbc1d
fieldpalmer <gfieldpalmer@gmail.com>
2019-01-07 12:52:20 -0500

    redux setup to register user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,16 @@
-import { TEST_DISPATCH } from "./types";
+import axios from "axios";
+import { GET_ERRORS } from "./types";
 
 // Register User
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+export const registerUser = (userData, history) => dispatch => {
+  // err catch uses redux-thunk dispatch method as middleware to put in redux state
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
