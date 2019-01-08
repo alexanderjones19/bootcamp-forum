51547ab0878127431faf7f48c4a676c0f4d2fdaf
aimontano <aimorales.montano@gmail.com>
2019-01-08 13:46:38 -0500

    Improve user experience

--- client/src/components/Forum/components/Header.js
+++ client/src/components/Forum/components/Header.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
+// this makes sure it gives the user a one page application experience
+import { NavLink as RRNavLink } from 'react-router-dom';
 
 
 class Header extends Component{
@@ -10,20 +12,20 @@ class Header extends Component{
     <Nav pills>
       <NavItem>  
         {window.location.pathname === '/discussion/general' ? 
-          <NavLink href="/discussion/general" active>General</NavLink> :
-          <NavLink href="/discussion/general">General</NavLink>
+          <NavLink to="/discussion/general" tag={RRNavLink} active>General</NavLink> :
+          <NavLink to="/discussion/general" tag={RRNavLink}>General</NavLink>
         }
       </NavItem>
       <NavItem>
         {window.location.pathname === "/discussion/code" ?
-          <NavLink href="/discussion/code" active>Code</NavLink> : 
-          <NavLink href="/discussion/code">Code</NavLink> 
+          <NavLink to="/discussion/code" tag={RRNavLink} active>Code</NavLink> : 
+          <NavLink to="/discussion/code" tag={RRNavLink}>Code</NavLink> 
         }
       </NavItem>
       <NavItem>
         {window.location.pathname === "/discussion/bootcamp" ?
-          <NavLink href="/discussion/bootcamp" active>Bootcamp</NavLink> : 
-          <NavLink href="/discussion/bootcamp">Bootcamp</NavLink> 
+          <NavLink to="/discussion/bootcamp" tag={RRNavLink} active>Bootcamp</NavLink> : 
+          <NavLink to="/discussion/bootcamp" tag={RRNavLink}>Bootcamp</NavLink> 
         }
       </NavItem>
     </Nav>
