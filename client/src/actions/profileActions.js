4bf9ed4545d1ce19015e1483d79d149b6666b2ad
fieldpalmer <gfieldpalmer@gmail.com>
2019-01-09 17:40:44 -0500

    profile actions working, all member should show but do no

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
@@ -22,6 +23,25 @@ export const getCurrentProfile = () => dispatch => {
       dispatch({
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+// get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,7 +40,7 @@ router.get(
   }
 );
 
-// @route      GET api/profiles/all
+// @route      GET api/profile/all
 // @desc       Get all profiles
 // @access     Public
 router.get("/all", (req, res) => {
