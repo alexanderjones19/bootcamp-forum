8bff5551909e38ee540bee71f83a88b4766d9ffd
aimontano <aimorales.montano@gmail.com>
2019-01-11 14:52:09 -0500

    able to view user image and name

--- client/src/components/Forum/components/ViewPost.js
+++ client/src/components/Forum/components/ViewPost.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { Media, Card, CardBody, CardTitle, CardText } from 'reactstrap';
+
+const userLogo = {
+  width: 60,
+  borderRadius: '100%',
+  marginBottom: 10,
+  marginRight: 10
+}
+
+const userName = {
+  marginTop: 5
+}
 
 const ViewPost = props => {
   let post = props.discussion[props.match.params.id - 1];
+  console.log(props)
   return (
-    <div className="mt-3 card">
-      <div className="card-body">
-        <h2 className="card-title">{post.postTitle}</h2>
-        <p>{post.postDescription}</p>
-        <p>{post.postContent}</p>
-      </div>
+    <div className="mt-3">
+        <Media>
+          <Media left href="#">
+            <Media object src={post.userImage} style={userLogo}/>
+          </Media>
+          <Media body>
+            <Media heading>
+              <Media href="#" style={userName}>{post.userName}</Media>
+            </Media>
+          </Media>
+        </Media>
+        <Card >
+        <CardBody>
+          <h4><CardTitle>{post.postTitle}</CardTitle></h4>
+          {/* Media part */}
+          <CardText>{post.postDescription}</CardText>
+          <CardText>{post.postContent}</CardText>
+        </CardBody>
+      </Card>
     </div>
   )
 }
