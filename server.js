d9bef43d1bd4ffdc2678b5ec46708238f9e99983
fieldpalmer <gfieldpalmer@gmail.com>
2019-01-12 17:45:30 -0500

    working app all components functioning except heroku deploy

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 app.use("/api/forum", forum);
 
-// serve static assets if in production
+// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
-  // set static folder
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
