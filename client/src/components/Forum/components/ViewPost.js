41c7f6fc6ec2dc65772be00f0701243a75bf5ac2
aimontano <aimorales.montano@gmail.com>
2019-01-14 20:36:49 -0500

    forgot what iw as working on

--- client/src/components/Forum/components/ViewPost.js
+++ client/src/components/Forum/components/ViewPost.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button, Media, Card, CardBody, CardTitle, CardText, CardFooter, CardHeader } from 'reactstrap';
+import ReactHtmlParser from 'react-html-parser';
+import Prism from 'prismjs';
+import '../../../prism.css';
+
+let commonmark = require('commonmark');
 
 const userLogo = {
   width: 60,
@@ -12,35 +17,51 @@ const userName = {
   marginTop: 5
 }
 
-const ViewPost = props => {
-  let post = props.discussion[props.match.params.id - 1];
-  console.log(props)
-  return (
-    <div className="mt-3">
-        <Card >
-          <CardHeader>
-            <Media>
-              <Media left href="#">
-                <Media object src={post.userImage} style={userLogo}/>
-              </Media>
-              <Media body>
-                <Media heading>
-                  <Media href="#" style={userName}>{post.userName}</Media>
+const convertMarkDown = text => {
+  let reader = new commonmark.Parser();
+  let writer = new commonmark.HtmlRenderer();
+
+  let parsed = reader.parse(text);
+  let result = writer.render(parsed);
+
+  return result;
+}
+
+class ViewPost extends Component {
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
+  render(){
+  let post = this.props.discussion[this.props.match.params.id - 1];
+    // {console.log(this.props)}
+    return (
+      <div className="mt-3">
+          <Card >
+            <CardHeader>
+              <Media>
+                <Media left href="#">
+                  <Media object src={post.userImage} style={userLogo}/>
+                </Media>
+                <Media body>
+                  <Media heading>
+                    <Media href="#" style={userName}>{post.userName}</Media>
+                  </Media>
                 </Media>
               </Media>
-            </Media>
-          </CardHeader>        
-        <CardBody>
-          <h4><CardTitle>{post.postTitle}</CardTitle></h4>
-          <CardText>{post.postDescription}</CardText>
-          <CardText>{post.postContent}</CardText>
-        </CardBody>
-        <CardFooter>
-          <Button color="primary">Reply</Button>
-        </CardFooter>
-      </Card>
-    </div>
-  )
+            </CardHeader>        
+          <CardBody>
+            <h4><CardTitle>{post.postTitle}</CardTitle></h4>
+            <CardText>{post.postDescription}</CardText>
+            {ReactHtmlParser(convertMarkDown(post.postContent))}
+          </CardBody>
+          <CardFooter>
+            <Button color="primary">Reply</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default ViewPost;
