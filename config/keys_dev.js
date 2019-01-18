60d89fbfebddb0ba2748accd52a1d06a7d7dfd81
alexanderjones19 <alexanderjones4215@gmail.com>
2019-01-17 23:17:28 -0500

    updated deployment variables

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,7 @@
 module.exports = {
+  //development database
   // mongoURI: "mongodb://buster:buster44@ds031581.mlab.com:31581/bootcamperbust",
+  //deployment database
   mongoURI: 'mongodb://heroku_3lgpj2r8:q6ko98m0n5uudsvf7298trj3ap@ds159164.mlab.com:59164/heroku_3lgpj2r8',
   secretOrKey: "secret"
 };
